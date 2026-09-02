35a787b295170749634d110fa2c28d0249db94cb
Sarafasra <sarafasraoui75@gmail.com>
2026-09-02 13:55:45 +0100

    Ajouter le modèle Department et sa relation avec les utilisateurs

--- backend/app/Models/Department.php
+++ backend/app/Models/Department.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nom',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
