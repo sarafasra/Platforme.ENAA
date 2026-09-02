b47875fcb3cd9ab235d05ef4acb809168a1a0a41
Sarafasra <sarafasraoui75@gmail.com>
2026-09-02 13:57:11 +0100

    Ajouter le modèle DemandeConge et ses relations

--- backend/app/Models/DemandeConge.php
+++ backend/app/Models/DemandeConge.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DemandeConge extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type_conge_id',
+        'date_debut',
+        'date_fin',
+        'type_journee',
+        'motif',
+        'statut',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date_debut' => 'date',
+            'date_fin' => 'date',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function typeConge()
+    {
+        return $this->belongsTo(TypeConge::class);
+    }
+
+    public function remplacement()
+    {
+        return $this->hasOne(Remplacement::class);
+    }
 }
