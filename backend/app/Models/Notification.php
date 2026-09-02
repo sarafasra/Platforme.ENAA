01b6f65f2f3bffd151f650c4c4e80ad7773f74f3
Sarafasra <sarafasraoui75@gmail.com>
2026-09-02 13:58:26 +0100

    Ajouter le modèle Notification et sa relation

--- backend/app/Models/Notification.php
+++ backend/app/Models/Notification.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'message',
+        'type',
+        'date_lecture',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date_lecture' => 'datetime',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
