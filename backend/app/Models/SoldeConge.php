939bee140bba4c5b1636ba341e6dbbbecaaebd7a
Sarafasra <sarafasraoui75@gmail.com>
2026-09-02 13:57:35 +0100

    Ajouter le modèle SoldeConge et ses relations

--- backend/app/Models/SoldeConge.php
+++ backend/app/Models/SoldeConge.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SoldeConge extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type_conge_id',
+        'jours_total',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function typeConge()
+    {
+        return $this->belongsTo(TypeConge::class);
+    }
 }
