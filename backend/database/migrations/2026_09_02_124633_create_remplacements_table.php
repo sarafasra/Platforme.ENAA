67e3159963e7d45151f75f0b88c1da16b4b966a8
Sarafasra <sarafasraoui75@gmail.com>
2026-09-02 13:51:45 +0100

    Créer la table des remplacements

--- backend/database/migrations/2026_09_02_124633_create_remplacements_table.php
+++ backend/database/migrations/2026_09_02_124633_create_remplacements_table.php
@@ -6,20 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('remplacements', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('demande_conge_id')
+                ->unique()
+                ->constrained('demandes_conges')
+                ->cascadeOnDelete();
+
+            $table->string('nom');
+            $table->date('date_debut');
+            $table->date('date_fin');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('remplacements');
