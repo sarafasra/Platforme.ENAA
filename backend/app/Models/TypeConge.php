27350113f05ecbe95fdb717f9600ea9409a82f75
Sarafasra <sarafasraoui75@gmail.com>
2026-09-02 13:56:08 +0100

    Ajouter le modèle TypeConge et ses relations

--- backend/app/Models/TypeConge.php
+++ backend/app/Models/TypeConge.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TypeConge extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'description',
+    ];
+
+    public function demandesConge()
+    {
+        return $this->hasMany(DemandeConge::class);
+    }
+
+    public function soldesConge()
+    {
+        return $this->hasMany(SoldeConge::class);
+    }
 }
