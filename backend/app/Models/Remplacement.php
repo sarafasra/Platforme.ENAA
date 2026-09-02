cedf60ce59cb5c2ef38073f93c3fed2d305ad066
Sarafasra <sarafasraoui75@gmail.com>
2026-09-02 13:58:50 +0100

    Ajouter le modèle Remplacement et sa relation

--- backend/app/Models/Remplacement.php
+++ backend/app/Models/Remplacement.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Remplacement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'demande_conge_id',
+        'nom',
+        'date_debut',
+        'date_fin',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date_debut' => 'date',
+            'date_fin' => 'date',
+        ];
+    }
+
+    public function demandeConge()
+    {
+        return $this->belongsTo(DemandeConge::class);
+    }
 }
