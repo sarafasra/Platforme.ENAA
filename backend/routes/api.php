dd8fbac943585307e253e7f9081dd5e56deb396e
Sarafasra <sarafasraoui75@gmail.com>
2026-09-04 09:53:34 +0100

    Ajouter les routes API des soldes de congé

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,7 +10,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/user', [AuthController::class, 'user']);  
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('types-conges', TypeCongeController::class);
     Route::apiResource('soldes-conges', SoldeCongeController::class);
